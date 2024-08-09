b8a194701e7c81c9f4783ee330a7bb7c6e55d591
EC2 Default User <pppike.0131@gmail.com>
2024-08-09 14:02:02 +0000

    create ec2 vpc on dev & prod

---
+++ udemy-terraform/modules/ec2/main.tf
@@ -0,0 +1,52 @@
+resource "aws_instance" "this" {
+  ami           = "ami-0a0b7b240264a48d7"
+  instance_type = "t2.micro"
+  
+  vpc_security_group_ids = [aws_security_group.this.id]
+  
+  user_data = file("${path.module}/user_data.sh")
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "udemy-terraform-ec2"
+  }
+}
+
+resource "random_id" "this" {
+  byte_length = 8
+}
+
+resource "aws_security_group" "this" {
+  name = "udemy-terraform-ec2-sg-${random_id.this.hex}"
+}
+
+resource "aws_security_group_rule" "ssh" {
+  count = var.allow_ssh ? 1 : 0
+
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
+
+resource "aws_security_group_rule" "http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
+
+# Nginxをインストールするため、EC2からの通信のあらゆるIPやプロトコルを許可
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
---
+++ udemy-terraform/modules/ec2/variables.tf
@@ -0,0 +1,5 @@
+variable "allow_ssh" {
+    type = bool
+    
+    default = false
+}
